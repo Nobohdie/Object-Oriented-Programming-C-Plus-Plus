c4325c5c05897172b8e574b47a4e65b1b7653e87
Nobohdie <tomorr2005@gmail.com>
2025-01-24 18:40:58 -0800

    created hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+/**
+ * {Lab 1: setting up IDE}
+ */
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello world" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
